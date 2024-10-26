07f9b75e19bacae9ccb6a57ef3dfe9c00e2d869a
unknown <ohidujjamanfahad987@gmail.com'git config --global user.name fahad>
2024-10-27 01:36:23 +0600

    text color change

--- src/components/movieDetails/MovieDetails.tsx
+++ src/components/movieDetails/MovieDetails.tsx
@@ -10,14 +10,14 @@ const MovieDetails = ({ movie, cast }: any) => {
                 <img src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`} className=' w-full h-full' />
             </div>
             <div className='border p-2 rounded-lg'>
-                <h1 className='text-[28px] font-bold text-[#121212]'>{movie?.title}</h1>
+                <h1 className='text-[28px] font-bold dark:text-white text-[#121212]'>{movie?.title}</h1>
                 <div className=' flex justify-between items-center mb-4'>
                     <p className=' text-sm'>{movie?.tagline}</p>
                     <p className='text-sm font-bold'>Release On : {movie?.release_date}</p>
                 </div>
                 <p className='text-[16px]'>{movie?.overview}</p>
-                <div className='flex text-sm font-bold text-[#121212]'>Geners :{movie?.genres?.map((gener: any) => <p className='text-sm font-bold text-[#121212]' key={gener?.id}> {gener?.name} {', '}</p>)}</div>
-                <p className='text-[18px] text-[#121212] font-bold my-2'>Cast : </p>
+                <div className='flex text-sm font-bold dark:text-white text-[#121212]'>Geners :{movie?.genres?.map((gener: any) => <p className='text-sm font-bold text-[#121212]' key={gener?.id}> {gener?.name} {', '}</p>)}</div>
+                <p className='text-[18px] dark:text-white text-[#121212] font-bold my-2'>Cast : </p>
                 <MovieCast actors={cast} />
             </div>
         </div>
